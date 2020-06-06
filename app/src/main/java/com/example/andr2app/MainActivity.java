ca3d312d6c845a06bfa5553a7ac55d77f3cc6ff7
i385085 <l.pashayanov@student.fontys.nl>
2020-06-06 16:33:12 +0200

    users updating on map when moveming

--- app/src/main/java/com/example/andr2app/MainActivity.java
+++ app/src/main/java/com/example/andr2app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.andr2app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,17 +20,20 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.gifdecoder.StandardGifDecoder;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -40,6 +44,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -48,7 +53,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
@@ -57,7 +65,11 @@ import com.google.firebase.storage.StorageReference;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -65,7 +77,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private Toolbar mainToolbar;
     private FloatingActionButton addItemBtn;
-
+    public double m_latitude;
+    public double m_longitude;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private Boolean mLocationPermissionsGranted = false;
     private final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
@@ -77,6 +90,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private StorageReference storageReference;
     private FirebaseFirestore firebaseFirestore;
     FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -101,12 +115,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         addItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent addNewItemIntent = new Intent(getApplicationContext(), AddItemActivity.class);
-               startActivity(addNewItemIntent);
+                Intent addNewItemIntent = new Intent(getApplicationContext(), AddItemActivity.class);
+                startActivity(addNewItemIntent);
             }
         });
         GetLocationPermission();
-
         // step counter:
         stepCounterTextView = findViewById(R.id.stepCounter_tv);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -116,31 +129,85 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         storageReference = FirebaseStorage.getInstance().getReference();
         firebaseFirestore = FirebaseFirestore.getInstance();
         getUsersPhotos();
-
+        SetFirestoreListener();
     }
 
-    private void getUsersPhotos(){
+    private  void SetFirestoreListener()
+    {
+        CollectionReference ref = firebaseFirestore.collection("Users");
+        ref.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@javax.annotation.Nullable QuerySnapshot snapshot, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w("FAIL!!!!", "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null) {
+//                    Toast toast = Toast.makeText(getApplicationContext(), "Users moving",
+//                            Toast.LENGTH_LONG);
+//                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+//                    toast.show();
+                    getUsersPhotos();
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Fail updating map",
+                            Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+            }
+        });
+    }
+
+    Dictionary<String,Marker> userMarkers = new Hashtable<String, Marker>();
+    public void ClearCurrentPhotos(String imageURL)
+    {
+       if(userMarkers.get(imageURL) != null)
+       {
+        userMarkers.get(imageURL).remove();
+        userMarkers.remove(imageURL);
+       }
+//int counter = 0;
+//            String bitmapToRemove=null;
+//            Marker markerToRemove=null;
+//                for (String url:
+//                        imagesURL) {
+//
+//                    if(url == imageURL)
+//                    {
+//                        bitmapToRemove = url;
+//                        markerToRemove = markersOnMap.get(counter);
+//                        markerToRemove.remove();
+//                    }
+//                    counter++;
+//                }
+//                markersOnMap.remove(markerToRemove);
+//                imagesURL.remove(bitmapToRemove);
+    }
+    private void getUsersPhotos() {
 
         CollectionReference ref = firebaseFirestore.collection("Users");
-       ref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-           @Override
-           public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots)
-                {
+
+        ref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
                     String image = documentSnapshot.get("image").toString();
                     String name = documentSnapshot.get("name").toString();
                     String latitude = documentSnapshot.get("latitude").toString();
                     String longitude = documentSnapshot.get("longitude").toString();
                     SetUserLocationMarkers setLocations = new SetUserLocationMarkers();
-                    setLocations.execute(image,name,latitude,longitude);
+                    setLocations.execute(image, name, latitude, longitude);
+
                 }
-           }
-       });
+
+            }
+        });
     }
 
 
-    class SetUserLocationMarkers extends AsyncTask<String,Void,Void>
-    {
+    class SetUserLocationMarkers extends AsyncTask<String, Void, Void> {
         @Override
         protected void onPreExecute() {
             // TODO Auto-generated method stub
@@ -152,13 +219,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Double userLatitude;
         Double userLongitude;
         Bitmap bitmapImage;
+
         @Override
         protected Void doInBackground(String... params) {
             userName = params[1];
             userLatitude = Double.parseDouble(params[2]);
             userLongitude = Double.parseDouble(params[3]);
             Uri mainImageURI = Uri.parse(params[0]);
-            URL imageURL=null;
+            URL imageURL = null;
             try {
                 imageURL = new URL(mainImageURI.toString());
             } catch (MalformedURLException e) {
@@ -172,18 +240,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Void result) {
-
             super.onPostExecute(result);
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(userLatitude, userLongitude))
-                    .title(userName)
-                    .snippet("") // products count
-                    .icon(BitmapDescriptorFactory.fromBitmap(bitmapImage)));
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(new LatLng(userLatitude, userLongitude));
+            markerOptions.title(userName);
+            markerOptions.snippet(""); // products count
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmapImage));
+            Marker marker = mMap.addMarker(markerOptions);
+            ClearCurrentPhotos(userName);
+            userMarkers.put(userName,marker);
         }
     }
-
 
 
     private SensorEventListener mSensorEventListener = new SensorEventListener() {
@@ -198,28 +268,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 steps++;
             }
             stepCounterTextView.setText("Steps: " + Integer.toString(steps));
+
         }
     };
 
     @Override
-    public void onResume(){
+    public void onResume() {
         sensorManager.registerListener(mSensorEventListener, sensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
         super.onResume();
         GetLocationPermission();
     }
-    public void initMap()
-    {
+
+    public void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
-        Log.d("msg: ", "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d("msg: ", "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
         //other markers with photos:
-        final LatLng MELBOURNE = new LatLng(latLng.latitude, + latLng.longitude);
+        final LatLng MELBOURNE = new LatLng(latLng.latitude, +latLng.longitude);
     }
 
     public void statusLocationCheck() {
@@ -227,8 +298,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
-
         }
+
+    }
+    public void OnLocationChangeInit()
+    {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            Log.d("TAPAK NE RABOTI", "statusLocationCheck: NOT WORKING");
+            return;
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // TODO Auto-generated method stub
+                SaveLocationOfUser(String.valueOf(location.getLatitude()),String.valueOf(location.getLongitude()));
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status,
+                                        Bundle extras) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+    public void SaveLocationOfUser(String lng, String lat) {
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("latitude", lat);
+        userMap.put("longitude", lng);
+
+        firebaseFirestore.collection("Users").document(user.getUid()).update(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+//                    Toast toast = Toast.makeText(getApplicationContext(), "User settings updated",
+//                            Toast.LENGTH_LONG);
+//                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+//                    toast.show();
+                } else {
+                    String error = task.getException().getMessage();
+                    Toast toast = Toast.makeText(getApplicationContext(), "Firestore error: " + error,
+                            Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+            }
+        });
     }
 
     private void buildAlertMessageNoGps() {
@@ -248,7 +376,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         final AlertDialog alert = builder.create();
         alert.show();
     }
-    public void GetDeviceLocation()
+
+    public void GetDeviceLocationAndZoom()
     {
         Log.d("msg: ", "getDeviceLocation: getting the devices current location");
 
@@ -265,8 +394,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             Log.d("message:", "onComplete: found location!");
                             Location currentLocation = (Location) task.getResult();
                                 if(currentLocation != null)
-                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                {
+                                    m_latitude = currentLocation.getLatitude();
+                                    m_longitude = currentLocation.getLongitude();
+                                    SaveLocationOfUser(String.valueOf(currentLocation.getLatitude()),String.valueOf(currentLocation.getLongitude()));
+                                    moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
                                     18);
+                                    OnLocationChangeInit();
+                                }
                                 else
                                 {
                                     statusLocationCheck();
@@ -309,7 +444,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         if (mLocationPermissionsGranted) {
-            GetDeviceLocation();
+            GetDeviceLocationAndZoom();
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
