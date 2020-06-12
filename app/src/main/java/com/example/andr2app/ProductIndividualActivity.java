4a33b482b64f196b8c4fd58b0c6bf94f8dad8a6f
Kuyumdzhiev,Anastas A.A <a.kuyumdzhiev@student.fontys.nl>
2020-06-12 03:46:16 +0200

    notification attempt

--- app/src/main/java/com/example/andr2app/ProductIndividualActivity.java
+++ app/src/main/java/com/example/andr2app/ProductIndividualActivity.java
@@ -2,7 +2,11 @@ package com.example.andr2app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +17,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -24,14 +35,20 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductIndividualActivity extends AppCompatActivity {
 
     private Product p;
-    private String userName = "";
-    private String userPhone = "";
+    private String loggedUserId = "";
 
     private ImageView ivProduct;
 
@@ -43,6 +60,11 @@ public class ProductIndividualActivity extends AppCompatActivity {
     private Button btnNotification;
 
     private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth mAuth;
+
+    private RequestQueue req;
+
+    private String url = "https://fcm.googleapis.com/fcm/send";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,14 +85,24 @@ public class ProductIndividualActivity extends AppCompatActivity {
         productName.setText(p.getName());
         productPrice.setText("Price: " + p.getPriceFormatted());
 
+        FirebaseMessaging.getInstance().subscribeToTopic(p.getId());
         btnNotification = findViewById(R.id.productIndividualNotify);
         btnNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               notifyInterest();
+                try {
+                    notifyInterest();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
+        mAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
+
+        loggedUserId = mAuth.getCurrentUser().getUid();
+
+        req = Volley.newRequestQueue(this);
 
         setUserInfo();
     }
@@ -93,7 +125,50 @@ public class ProductIndividualActivity extends AppCompatActivity {
         });
         }
 
-        void notifyInterest(){
-        
+        void notifyInterest() throws JSONException {
+
+            JSONObject mainObj = new JSONObject();
+
+            mainObj.put("to", "/topics/"+p.getId());
+
+            JSONObject notificationObject = new JSONObject();
+            notificationObject.put("title", "A user is interested in your item!");
+            notificationObject.put("body", "User " + loggedUserId + " will contact you about your " + p.getName());
+
+            mainObj.put("notification", notificationObject);
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, mainObj, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Notification sent!",
+                            Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Error with notification",
+                            Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+            }
+            ){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> header = new HashMap<>();
+
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=AAAAoWuvyQg:APA91bFimKsHznbT5e2oCJJRTI_trT675ELfhRRqiCtxnJ4xEkNosd-I5NKwAoyLzIN9npt2L9jX3FdFkqI32xz192EgQe09pfoTvn3ZPNOEaf3i_Sqr6LElz-wpl93_Re0bSm0ktjil");
+                    return header;
+                }
+            };
+
+            req.add(request);
         }
+
+
 }
+
+
